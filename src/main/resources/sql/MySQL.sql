5cb8be01ee092aab89bb17d1d0e9bf74e8f52292
wang926454 <1107224733@qq.com>
2019-01-24 18:19:29 +0800

    更新配置

--- src/main/resources/sql/MySQL.sql
+++ src/main/resources/sql/MySQL.sql
@@ -5,39 +5,39 @@ create database shirojwt;
 use shirojwt;
 
 create table user (
-id int primary key auto_increment COMMENT "ID",
-account varchar(20) not null unique COMMENT "帐号",
-password varchar(80) not null COMMENT "密码",
-username varchar(20) not null COMMENT "昵称",
-reg_time datetime not null COMMENT "注册时间"
+  id int primary key auto_increment COMMENT "ID",
+  account varchar(20) not null unique COMMENT "帐号",
+  password varchar(80) not null COMMENT "密码",
+  username varchar(20) not null COMMENT "昵称",
+  reg_time datetime not null COMMENT "注册时间"
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT "用户表";
 
 CREATE TABLE role (
-id int primary key auto_increment COMMENT "ID",
-name varchar(128) not null unique COMMENT "角色名称"
+  id int primary key auto_increment COMMENT "ID",
+  name varchar(128) not null unique COMMENT "角色名称"
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT "角色表";
 
 
 CREATE TABLE permission (
-id int primary key auto_increment COMMENT "ID",
-name varchar(128) COMMENT '资源名称',
-per_code varchar(128) not null unique COMMENT '权限代码字符串'
+  id int primary key auto_increment COMMENT "ID",
+  name varchar(128) COMMENT '资源名称',
+  per_code varchar(128) not null unique COMMENT '权限代码字符串'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT "资源表";
 
 CREATE TABLE user_role (
-id int primary key auto_increment COMMENT "ID",
-user_id int not null COMMENT '用户id',
-role_id int not null COMMENT '角色id',
-foreign key (user_id) references user (id),
-foreign key (role_id) references role (id)
+  id int primary key auto_increment COMMENT "ID",
+  user_id int not null COMMENT '用户id',
+  role_id int not null COMMENT '角色id',
+  foreign key (user_id) references user (id),
+  foreign key (role_id) references role (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT "用户角色表";
 
 CREATE TABLE role_permission (
-id int primary key auto_increment COMMENT "ID",
-role_id int not null COMMENT '角色id',
-permission_id int not null COMMENT '权限id',
-foreign key (role_id) references role (id),
-foreign key (permission_id) references permission (id)
+  id int primary key auto_increment COMMENT "ID",
+  role_id int not null COMMENT '角色id',
+  permission_id int not null COMMENT '权限id',
+  foreign key (role_id) references role (id),
+  foreign key (permission_id) references permission (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT "角色资源表";
 
 /* 密码都是帐号 */
